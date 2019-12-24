fd7c34d9d4340d04b12deaee777448d2bcbb5547
Matheus Barth <mfbarth@localhost.localdomain>
2019-12-24 12:48:34 -0300

    Aula 03 - A Ligaçao entre açoes do usuario e o modelo

---
+++ client/js/controllers/NegociacaoController.js
@@ -0,0 +1,33 @@
+class NegociacaoController {
+
+    constructor(){
+
+        let $ = document.querySelector.bind(document);
+        
+        this._inputData = $('#data');
+        this._inputQuantidade = $('#quantidade');
+        this._inputValor = $("#valor");
+    }
+
+    adiciona(event) {
+        event.preventDefault();
+
+        let data = new Date(
+            ...this._inputData.value.split('-')
+            .map((item, index) => item - index % 2)
+        );
+
+        let negociacao = new Negociacao(
+            data,
+            this._inputQuantidade.value,
+            this._inputValor.value
+        );
+
+        console.log(negociacao);
+
+        this._inputData.value = null;
+        this._inputQuantidade = 1;
+        this._inputValor.value = 0;
+        this._inputData.focus();
+    }
+}
